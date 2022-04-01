b3e4393f45205d3ded89a054a7b867d5a8bdafbf
ossa1992 <osaiorra@gmail.com>
2022-04-01 10:28:38 +0700

    menambahkan web.php,stylebuku.css,v_about.php,v_footer.php,v_header.php,v_index.php

---
+++ v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Ossa F.</li>
+ <li>Nama Belakang : Ibrahim</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan Ranca dulang NO.90 Karawaci</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Bandung</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Bersepeda</li>
+ <li>muaythai</li>
+ </ul>
+</section>
